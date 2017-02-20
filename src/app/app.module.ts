53dd9504473762ef7d156c312fa86246675fb430
daredkar <DRE@tdc.dk>
2017-02-20 08:42:30 +0100

    Angular2_Bootstrap3 first commit
    Angular2_Bootstrap3 first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { NavbarComponent } from './navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent }  from './app.component';
+@NgModule({
+
+	imports:      [ BrowserModule, FormsModule, AppRoutingModule],
+	declarations: [ AppComponent, NavbarComponent,HomeComponent],
+	bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
