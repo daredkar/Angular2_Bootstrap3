8d1507f44303cb1484fcdd93e8bdd371e3d2c2fe
daredkar <redkar.darshan11@gmail.com>
2017-02-24 17:31:30 +0530

    we have read and displayed the CSV file on browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'; 
+import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar.component';
 import { HomeComponent } from './home/home.component';
 import { Http, Response } from '@angular/http';
@@ -8,25 +8,25 @@ import { PageToRead } from './page_to_read';
 	selector: 'my-app',
 	templateUrl: 'app/app.html'
 })
-export class AppComponent { 
+export class AppComponent {
 
 	pages : PageToRead[] = [];
 
 	constructor (private http: Http) {}
-	onChange(event) {
+	onChange(event :any) {
         var text : PageToRead[] = [];
         var files = event.srcElement.files;
         if(files[0].name.includes(".csv"))
         {
            var input = event.target;
            var reader = new FileReader();
-         
+
            reader.onload = function(){
              let csvData = reader.result;
              let allTextLines = csvData.split(/\r\n|\n/);
              let headers = allTextLines[0].split(';');
              let lines = [];
-             
+
              for (let i = 0; i < allTextLines.length; i++) {
               // split content based on comma
               let data = allTextLines[i].split(';');
@@ -36,7 +36,7 @@ export class AppComponent {
                   tarr.push(data[j]);
                 }
                 var page = new PageToRead();
-                page.Url = "https://www.google.ie/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#safe=active&q=" + tarr[0] + "+" + tarr[1] + "+" + tarr[2];
+                page.Url = "http://localhost:4600" + tarr[0] + "+" + tarr[1] + "+" + tarr[2];
                 text.push(page);
               }
             }
